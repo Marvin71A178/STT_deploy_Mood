91e756f438e25e05b18ba00cefb2e1b2b01e1e36
Marvin <aazz282828@gmail.com>
2024-05-03 14:22:19 +0800

    app cors options

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import os , sys
+import os
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-import  Mood.predict as mood_pd
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import asyncio
 from starlette.concurrency import run_in_threadpool
 import uvicorn
+import Mood.predict as mood_pd
 
 app = FastAPI()
+
+# 設定允許的來源
 origins = [
     "http://localhost:3000",
     "http://localhost:8080",
 ]
 
+# 添加CORS中介軟體
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -21,23 +22,30 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-class process_Mood_pd(BaseModel):
+
+# 定義請求體模型
+class ProcessMoodRequest(BaseModel):
     TestData: str
 
+# 根路由，歡迎訊息
 @app.get("/")
 async def root():
     return {"message": "Welcome to the AudioCraft API. Use /mood_analyze/ to perform mood analysis and /music_generate/ to generate music based on mood."}
 
+# 處理 OPTIONS 請求
+@app.options("/mood_analyze/")
+async def options_mood_analyze():
+    return {}
+
+# 情緒分析路由
 @app.post("/mood_analyze/")
-async def perform_mood_pd(request: process_Mood_pd):
+async def perform_mood_pd(request: ProcessMoodRequest):
     try:
         result = await run_in_threadpool(mood_pd.prediction, request.TestData)
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
-    uvicorn.run(app , host = '0.0.0.0' , port = port)
-    
+    port = int(os.getenv('PORT', 80))
+    uvicorn.run(app, host='0.0.0.0', port=port)
